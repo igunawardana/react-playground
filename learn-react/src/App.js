702d40c124c4dde4b01dd0f596863da4955548cf
Isuru Gunawardana <isuru@Isurus-mbpc.local>
2019-11-05 01:10:46 +0530

    breakdown with components

---
+++ learn-react/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Footer from "./Footer"
+
+function App() {
+    return (
+        <div>
+            <nav>
+                <h1>Hellow world</h1>
+                <ul>
+                    <li>one</li>
+                    <li>two</li>
+                    <li>three</li>
+                </ul>
+            </nav>
+            <main>This is where main content will go </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ learn-react/src/componenets/MyInfo.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Hi Isuru</h1>
+            <p>I'm learning react</p>
+        </div>)
+}
+
+export default MyInfo
