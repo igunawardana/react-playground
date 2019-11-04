3c78b321ae67ef98f916a4b632351fbedf9b26d2
Isuru Gunawardana <isuru@Isurus-mbpc.local>
2019-11-05 01:42:44 +0530

    function and vlalues

--- learn-react-ex1/src/MainContent.js
+++ learn-react-ex1/src/MainContent.js
@@ -1,8 +1,20 @@
 import React from "react"
 
 function MainContent() {
+
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    if (hours < 12) {
+        timeOfDay = "Morning"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "Afternoon"
+    } else {
+        timeOfDay = "Night"
+    }
     return (
         <div>
+            <h2>Good {timeOfDay} </h2>
             <h5>Select from below</h5>
             <input type="checkbox" value="Sinhala" /> Sinhala <br />
             <input type="checkbox" value="Spain" /> Spain <br />
