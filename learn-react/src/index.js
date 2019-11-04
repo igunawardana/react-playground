951141cc327690e92749a93a37c09e0d7dfc1362
Isuru Gunawardana <isuru@Isurus-mbpc.local>
2019-11-05 00:34:50 +0530

    functions to separate files

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,12 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-function MyInfo() {
-    return (
-        <div>
-            <h1>Hi Isuru</h1>
-            <p>I'm learning react</p>
-        </div>)
-}
+import MyInfo from "./componenets/MyInfo"
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"))
