fa2511909d156f6ec27648b60418ea63ca98c2ab
Isuru Gunawardana <isuru@Isurus-mbpc.local>
2019-11-05 01:47:08 +0530

    adding styles

--- learn-react-ex1/src/Header.js
+++ learn-react-ex1/src/Header.js
@@ -1,8 +1,12 @@
 import React from "react"
 
 function Header() {
+    const styles = {
+        color: "#FF8C00",
+        fontSize: 24
+    }
     return (
-        <header><h1>....Header....</h1></header>
+        <header><h1 style={styles}>....Header....</h1></header>
     )
 }
 
