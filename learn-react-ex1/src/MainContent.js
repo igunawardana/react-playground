4c9726e945d7ed3e66e398e5c29705c995757e58
Isuru Gunawardana <isuru@Isurus-mbpc.local>
2019-11-05 01:31:12 +0530

    exercise with checkbox

---
+++ learn-react-ex1/src/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <div>
+            <h5>Select from below</h5>
+            <input type="checkbox" value="Sinhala" /> Sinhala <br />
+            <input type="checkbox" value="Spain" /> Spain <br />
+            <input type="checkbox" value="German" /> German <br />
+            <input type="checkbox" value="English" /> English <br />
+        </div>
+    )
+}
+
+export default MainContent
